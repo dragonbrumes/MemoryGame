15e03494b642b8105319bc72cf030c264b518507
Stéphane aal <36472404+dragonbrumes@users.noreply.github.com>
2018-04-13 15:56:04 +0200

    same, but in the array + data-name

--- js/app.js
+++ js/app.js
@@ -9,6 +9,9 @@ var app = {
     //appelle de la méthode de génération des cards
     app.cardsGenerators();
 
+    //on méange les cartes
+    app.shuffleCards(app.cards);
+
     //appelle de la méthode d'ajout des cards au board
     app.addCardsToboard();
 
@@ -17,74 +20,95 @@ var app = {
 
   }, // *********************fin init
 
-    // méthode pour générer les cards ***************
-    cardsGenerators: function(){
-      var $i = 0;
-      //on boucle autant de fois que de carte à créer
-      while ($i < 36) {
-        $i++;
-        //création de 28 div
-        var $cardDiv = $('<div>');
+  // méthode pour générer les cards ***************
+  cardsGenerators: function(){
+    // appelle de création des x div
+    var $i = 0;
+    //on définit la position du background de .image avt la boucle
+    var $positionX = 100;
+    var $positionY = 0;
 
-        //affectation des class aux div
-        $cardDiv.addClass('card');
-        //rangement des divs .cards dans le tableau app.cards
-        app.cards[$i] = $cardDiv;
-      } // fin WHILE ********************
+    for (var index in app.cardsName) {
+      //console.log(app.cardsName[index]);
+      var $cardName = cardsName.name[index];
+    }
 
-    // ## créations des div enfants ##
-      //définition de la position pour les div images
-      var $positionY = 0;
-      //boucle sur les divs du tableau des .cards
-      app.cards.forEach(function(cards) {
+    while ($i < 36) {
 
-        // Création des Div .cache
-        var $cacheDiv = $('<div>');
-        $cacheDiv.addClass('cache');
+      //création des x div .card
+      var $cardDiv = $('<div>');
+      $cardDiv.addClass('card');
+      $cardDiv.data('idCard', cardsName.name[$i]); //on affecte un data  au cas où
+      //création des x div .cache
+      var $cacheDiv = $('<div>');
+      $cacheDiv.addClass('cache');
+      //création des x div .image
+      var $imageDiv = $('<div>');
+      $imageDiv.addClass('image');
+      $imageDiv.css({
+        backgroundImage: 'url(../images/cards.png)',
+        backgroundPosition: ' 100px '+$positionY+'px',
+      });
+      //compteur du while
+      $i++;
+      //on décrémente la position du background pour matcher avec l'odre descendant du sprit
+      //la 1ere position est donc 0 avant la décrémentation
+      $positionY -= 100;
+      //ajout des .cache et .image au .card
+      $cardDiv.append($cacheDiv);
+      $cardDiv.append($imageDiv);
 
-        // Création des Divs .image
-        $positionY += 100; //on incrémente de 100 la position
-        var $imageDiv = $('<div>');
-        $imageDiv.addClass('image');
-        $imageDiv.css({
-          backgroundImage: 'url(../images/cards.png)',
-          backgroundPosition: ' 100px '+$positionY+'px',
-        });
-        // ajout des divs .cache et .image aux divs .card du tableau app.cards
-        cards.append($cacheDiv);
-        cards.append($imageDiv);
+      //On range les div dans la property tableau app.card
+      app.cards.push($cardDiv);
+    }
+      console.log(app.cards);
+  },//**fin cardsGenerators
 
-      });//fin foreach
-      //console.log(app.cards[2]);
-      //console.log(app.cacheCards);
+  bourrinCardsGenerators: function(){
+    var $i = 0;
+    var $positionY = 0;
+    //on boucle autant de fois que de carte à créer
+    while ($i < 36) {
+      $i++;
+      //création de x div
+      var $cacheDiv = '<div class="cache"></div>';
+      $positionY += 100; //on incrémente de 100 la position
+      var $imageDiv = '<div class="image" style="background-image: url(../images/cards.png), background-position:  100px '+$positionY+'px;" ></div>';
+      //var $imageDiv = '<div class="image"></div>';
+      var $cardDiv = '<div class="card">'+$cacheDiv+''+$imageDiv+'</div>';
+      app.cards.push($cardDiv);
+      console.log(app.cards);
 
-      //positionnement des div enfants de la div card
-       // $('.card').append($cacheDiv);
-       // $('.card').append($imageDiv);
+    } // fin WHILE ********************
 
+  },//**fin cardsGenerators*****************
 
-    },//**fin cardsGenerators*****************
+  //mélange des cartes positions
+  shuffleCards: function(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  },//fin shuffle
 
+  //méthode d'ajout des div aux board *************
+  addCardsToboard:function(){
+    $('.board-game').append(app.cards);
+  }, // addCardsToboard
 
-    //méthode d'ajout des div aux board *************
-    addCardsToboard:function(){
-      $('.board-game').append(app.cards);
-    }, // addCardsToboard
+  returnCard: function(event){
+    //on récupère le data de la carte cliquée et identifiée
+    //var $clickedCard = $(this).data('nbr');
 
-    returnCard: function(event){
-      //on récupère le data de la carte cliquée et identifiée
-      //var $clickedCard = $(this).data('nbr');
+    //on cible la card qui a ce data
+    //var $selectedCard = $('.card').data('nbr');
 
-      //on cible la card qui a ce data
-      //var $selectedCard = $('.card').data('nbr');
+    //on retir à cette card la class .cache et remple par la classe .image
+    $(this).children().hide('cache');
+    $(this).children().next().show('image');
 
-      //on retir à cette card la class .cache et remple par la classe .image
-      $(this).children().hide('cache');
-      $(this).children().next().show('image');
-      console.log($(this));
-      // console.log($clickedCard);
-      // console.log($selectedCard);
-    }, // returnCard
+  }, // returnCard
 
 
 
---
+++ js/cardsName.js
@@ -0,0 +1,7 @@
+var cardsName = {
+name: [
+'pommeRouge', 'banane', 'orange', 'citronVert', 'ognion', 'peche', 'citron', 'fraise', 'pommeVerte', 'abricot', 'raisin', 'pasteque', 'abricotViolet', 'poire', 'cerise', 'framboise', 'mangue', 'ceriseJaune',
+'pommeRouge', 'banane', 'orange', 'citronVert', 'ognion', 'peche', 'citron', 'fraise', 'pommeVerte', 'abricot', 'raisin', 'pasteque', 'abricotViolet', 'poire', 'cerise', 'framboise', 'mangue', 'ceriseJaune'
+],
+
+}
